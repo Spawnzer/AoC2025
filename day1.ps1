42c4f32752b31691bcfeca9d9c11c2a452910697
adubeau <78969151+Spawnzer@users.noreply.github.com>
2024-12-03 09:49:19 -0500

    Update day1.ps1

--- day1.ps1
+++ day1.ps1
@@ -8,16 +8,14 @@ Get-Content("C:\Users\alex.dubeau\aoc\1\input.txt") | ForEach-Object {$line = $_
 $data1 = $data1 | sort
 $data2 = $data2 | sort
 
-foreach ($i in 0..$($data1.Count -1) ){
-  if ($data1[$i] -gt $data2[$i]) { $dif += $data1[$i]-$data2[$i] }
-  else { $dif += $data2[$i]-$data1[$i] }
-}
+foreach ($i in 0..$($data1.Count -1) ){ dif += [math]::abs($data1[$1]-$data2[$i]) }
 
 foreach ($n in $dif) { $phase1 += $n }
 
-foreach ($n in $data1){
+foreach ($n in $data1){ 
   $c = ($data2 | ? {$_ -eq $n}).count
-  if ($c -gt 0){$phase2 += [decimal]::Multiply($n,$c)}}
+  if ($c -gt 0){$phase2 += [decimal]::Multiply($n,$c) } 
+}
   
 $phase1
 $phase2
