aa2d2e8d6a7e499cf250673d9c5c72b6010cc805
adubeau <78969151+Spawnzer@users.noreply.github.com>
2024-12-03 17:04:28 -0500

    started p2

--- 2/day2.ps1
+++ 2/day2.ps1
@@ -6,7 +6,7 @@ Get-Content("C:\Users\alex.dubeau\aoc\2\input.txt") | ForEach-Object {$data += $
 
 foreach ($l in $data) {
       $temp = @()
-      foreach ($n in ($l -split "`n")) {
+      foreach ($n in $l) {
           $row = $l -split '\s+' | ForEach-Object { [int]$_ }
           $temp += @($row)
       }
@@ -16,15 +16,17 @@ foreach ($l in $data) {
 foreach ($line in $array){
   $ascending = $null
   $safe = $true
-  foreach ($i in 0..$($line.Count -2)){
+  $unsafe = 0
+  foreach ($i in 0..$($line.Count -1)){
       $dif = $line[$i + 1] - $line[$i]
       if ($ascending -eq $null){
             if ($dif -gt 0){$ascending = $true}
             elseif ($dif -lt 0){$ascending = $false}
-            else {$safe = $false; break}
+            else {$unsafe++}
       }
-      if ($ascending -and ($dif -lt 1 -or $dif -gt 3)) {$safe = $false; break}
-      if (-not $ascending -and ($dif -gt -1 -or $dif -lt -3)) {$safe = $false; break}
+      if ($ascending -and ($dif -lt 1 -or $dif -gt 3)) {$unsafe++}
+      if (-not $ascending -and ($dif -gt -1 -or $dif -lt -3)) {$unsafe++}
+      if ($unsafe -gt 1){ $safe = $false; break}
   }
   if ($safe) { $c += 1}
 }
