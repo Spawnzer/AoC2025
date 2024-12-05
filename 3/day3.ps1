dbbef86b40521958a494c37251855f15f2ac1d0c
adubeau <78969151+Spawnzer@users.noreply.github.com>
2024-12-05 07:43:44 -0500

    Update day3.ps1

--- 3/day3.ps1
+++ 3/day3.ps1
@@ -1,5 +1,3 @@
-$valid = Select-String -Path C:\Users\alex.dubeau\aoc\3\input.txt -Pattern $pattern -AllMatches | Select-String -pattern "(\d+)" -AllMatches
-$a = $valid | Select-String -pattern "(\d+)" -AllMatches | ? {$_.Matches.value}
-for ($i = 0; $i -lt $a.Length; $i++){
-$r+= ([int]$a[$i].matches.value[0] * [int]$a[$i].Matches.value[1])}
+$a = Select-String -Path C:\Users\alex.dubeau\aoc\3\input.txt -pattern "mul\((\d+),(\d+)\)" -AllMatches
+foreach ($match in $a.matches){ $r+= ([int]$match.groups[1].value * [int]$match.groups[2].value) }
 $r
