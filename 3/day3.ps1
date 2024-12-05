85381aa76d1b581b1921dbc3042f80c01f0521aa
adubeau <78969151+Spawnzer@users.noreply.github.com>
2024-12-05 13:20:05 -0500

    Update day3.ps1

--- 3/day3.ps1
+++ 3/day3.ps1
@@ -1,3 +1,15 @@
 $a = Select-String -Path C:\Users\alex.dubeau\aoc\3\input.txt -pattern "mul\((\d+),(\d+)\)" -AllMatches
 foreach ($match in $a.matches){ $r+= ([int]$match.groups[1].value * [int]$match.groups[2].value) }
 $r
+
+$r = @()
+$res = 0
+$a = get-content -Path C:\Users\alex.dubeau\aoc\3\input.txt 
+$str = $a -split '(?=do)'
+
+foreach ($s in $str){
+  if ($s.StartsWith('don')){}
+  else {$r += $s | Select-String  -Pattern "mul\((\d+),(\d+)\)" -AllMatches}
+}
+foreach ($match in $r.matches){ $res+= ([int]$match.groups[1].value * [int]$match.groups[2].value) }
+$res
